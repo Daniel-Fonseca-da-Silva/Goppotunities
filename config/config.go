2cd5f2d0def6147ab82880c1f55ff5dbda995b5c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2024-03-31 18:58:12 -0300

    feat: add database handler

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"errors"
+	"fmt"
 
 	"gorm.io/gorm"
 )
@@ -12,8 +12,19 @@ var (
 )
 
 func Init() error {
-	return errors.New("fake error")
-	// return nil
+	var err error
+
+	// Initialize SQlite
+	db, err = InitializeSQlite()
+
+	if err != nil {
+		return fmt.Errorf("Error initializing Sqlite: %v", err)
+	}
+	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
--- go.mod
+++ go.mod
@@ -2,11 +2,15 @@ module github.com/Daniel-Fonseca-da-Silva/Goppotunities
 
 go 1.22.1
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-gonic/gin v1.9.1
+	gorm.io/driver/sqlite v1.5.5
+)
 
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 )
 
 require (
