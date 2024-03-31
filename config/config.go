68326ddabb1e5cc4f49bc994a783e718d1570ca6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2024-03-31 17:02:31 -0300

    feat: add custom logger

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	return errors.New("fake error")
+	// return nil
+}
+
+func GetLogger(p string) *Logger {
+	// Initialize Logger
+	logger = NewLogger(p)
+	return logger
+}
--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/Daniel-Fonseca-da-Silva/Goppotunities/router"
+import (
+	"github.com/Daniel-Fonseca-da-Silva/Goppotunities/config"
+	"github.com/Daniel-Fonseca-da-Silva/Goppotunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize config
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	// Initialize router
 	router.Initialize()
 }
