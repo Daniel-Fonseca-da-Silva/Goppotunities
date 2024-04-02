b6a9a7296dc30e408dea60807911064b36e50028
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2024-04-02 13:39:16 -0300

    docs: add documentation for create opening

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -7,6 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// @Summary create opening
+// @Description Create a new job opening
+// @Tags openings
+// @Accept json
+// @Produce json
+// @Param request body CreateOpeningRequest true "Request body"
+// @Success 200 {object} CreateOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [post]
 func CreateOpeningHandler(ctx *gin.Context) {
 	request := CreateOpeningRequest{}
 	ctx.BindJSON(&request)
